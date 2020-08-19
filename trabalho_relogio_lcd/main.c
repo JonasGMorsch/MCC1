b086164be7487afbe9157962475275858cbab767
Jonas G. Morsch <jonasgmorsch@gmail.com>
2020-08-19 02:50:31 -0300

    Progresso no sensor ultrasom

--- trabalho_relogio_lcd/main.c
+++ trabalho_relogio_lcd/main.c
@@ -14,62 +14,66 @@
 #include "lcd.h"
 #include "relogio.h"
 
-void init_clock_system(){
+void init_clock_system()
+{
 
-    /* Configuração do MCLK em 8MHz */
+	/* Configuração do MCLK em 8MHz */
 
-    // Se calibração foi apagada, para aplicação
-    if (CALBC1_8MHZ==0xFF)
-        while(1);
+	// Se calibração foi apagada, para aplicação
+	if (CALBC1_8MHZ == 0xFF)
+		while (1)
+			;
 
-    DCOCTL = 0;
-    BCSCTL1 = CALBC1_8MHZ;
-    DCOCTL = CALDCO_8MHZ;
+	DCOCTL = 0;
+	BCSCTL1 = CALBC1_8MHZ;
+	DCOCTL = CALDCO_8MHZ;
 
-    /* Configura ACLK para usar VLO: ~10kHZ */
-    BCSCTL3 |= LFXT1S_2;
+	/* Configura ACLK para usar VLO: ~10kHZ */
+	BCSCTL3 |= LFXT1S_2;
 }
 
-void main(){
-    char string[12];
+void main()
+{
+	char string[12];
 
-    /* Configuração de hardware */
-    WDTCTL = WDTPW | WDTHOLD;
+	/* Configuração de hardware */
+	WDTCTL = WDTPW | WDTHOLD;
 
 #if defined (__MSP430FR2355__)
-    /* Disable the GPIO power-on default high-impedance mode */
-    PM5CTL0 &= ~LOCKLPM5;
+	/* Disable the GPIO power-on default high-impedance mode */
+	PM5CTL0 &= ~LOCKLPM5;
 #endif
 
-    /* Systema de clock e applicação */
-    init_clock_system();
-    init_relogio();
+	/* Systema de clock e applicação */
+	init_clock_system();
+	init_relogio();
 
-    /* Inicializa hardare: veja lcd.h para
-     * configurar pinos */
-    lcd_init_4bits();
+	/* Inicializa hardare: veja lcd.h para
+	 * configurar pinos */
+	lcd_init_4bits();
 
-    /* Escreve string */
-    lcd_send_data(LCD_LINE_1+5, LCD_CMD);
-    lcd_write_string("MSP430");
+	/* Escreve string */
+	lcd_send_data(LCD_LINE_1 + 5, LCD_CMD);
+	lcd_write_string("MSP430");
 
-    __bis_SR_register(LPM0_bits + GIE);
+	__bis_SR_register(LPM0_bits + GIE);
 
-    while (1)
-    {
-        lcd_send_data(LCD_LINE_0+4, LCD_CMD);
+	while (1)
+	{
+		lcd_send_data(LCD_LINE_0 + 4, LCD_CMD);
 
-        /* Para o snprintf funcionar com modificadores:
-         * Configuração do projeto -> MSP430 Compiler -> Edit Flags -> Mudar para: printf_support=nofloat   */
-        snprintf(string, 12, "%02d:%02d:%02d", obter_horas(), obter_minutos(), obter_segundos());
-        lcd_write_string(string);
-        //lcd_write_string("fdfvfv");
+		/* Para o snprintf funcionar com modificadores:
+		 * Configuração do projeto -> MSP430 Compiler -> Edit Flags -> Mudar para: printf_support=nofloat   */
+		snprintf(string, 12, "%02d:%02d:%02d", obter_horas(), obter_minutos(),
+					obter_segundos());
+		lcd_write_string(string);
+		//lcd_write_string("fdfvfv");
 
-        if (em_ajuste())
-           lcd_send_data('*', LCD_DATA);
-       else
-           lcd_send_data(' ', LCD_DATA);
+		if (em_ajuste())
+			lcd_send_data('*', LCD_DATA);
+		else
+			lcd_send_data(' ', LCD_DATA);
 
-        __bis_SR_register(LPM0_bits);
-    }
+		__bis_SR_register(LPM0_bits);
+	}
 }
